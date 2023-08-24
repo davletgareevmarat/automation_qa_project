992e70f557280699191e827768f4adbcf87bb9c8
davletgareevmarat <davletgareev.mrt@gmail.com>
2023-08-24 10:36:07 +0500

    update elements_test.py added test radiobutton

--- locators/elements_page_locators.py
+++ locators/elements_page_locators.py
@@ -22,3 +22,10 @@ class CheckBoxPageLocators:
     CHECKED_ITEMS = (By.CLASS_NAME, "rct-icon-check")
     TITLE_ITEM = ".//ancestor::span[@class='rct-text']"
     OUTPUT_RESULT = (By.CLASS_NAME, "text-success")
+
+
+class RadioButtonPageLocators:
+    YES_RADIOBUTTON = (By.CSS_SELECTOR, 'label[class^="custom-control"][for="yesRadio"]')
+    IMPRESSIVE_RADIOBUTTON = (By.CSS_SELECTOR, 'label[class^="custom-control"][for="impressiveRadio"]')
+    NO_RADIOBUTTON = (By.CSS_SELECTOR, 'label[class^="custom-control"][for="noRadio"]')
+    OUTPUT_RESULT = (By.CSS_SELECTOR, 'p span[class="text-success"]')
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,5 +1,6 @@
+import pytest
 
-from pages.elements_page import TextBoxPage, CheckBoxPage
+from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage
 
 
 class TestElements:
@@ -19,4 +20,23 @@ class TestElements:
             check_box_page.click_random_checkbox(25)
             input_checkbox = check_box_page.get_checked_checkboxes()
             output_result = check_box_page.get_checked_checkboxes()
-            assert input_checkbox == output_result, 'checkboxes have not been selected'
+            assert input_checkbox == output_result, "checkboxes have not been selected"
+
+    class TestRadioButton:
+        @pytest.mark.xfail(raises=AssertionError)
+        def test_radio_button(self, driver):
+            radio_button_page = RadioButtonPage(
+                driver, "https://demoqa.com/radio-button"
+            )
+            radio_button_page.open()
+            radio_button_page.click_on_the_radio_button("yes")
+            output_yes = radio_button_page.get_output_result()
+            radio_button_page.click_on_the_radio_button("impressive")
+            output_impressive = radio_button_page.get_output_result()
+            radio_button_page.click_on_the_radio_button("no")
+            output_no = radio_button_page.get_output_result()
+            assert output_yes == "Yes", "'Yes' have not been selected"
+            assert (
+                output_impressive == "Impressive"
+            ), "'Impressive' have not been selected"
+            assert output_no == "No", "'No' have not been selected"
