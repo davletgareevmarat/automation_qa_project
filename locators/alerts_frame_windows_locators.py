7dedac1cc5be22f15e4f3fdb91e2b969309c13d0
davletgareevmarat <davletgareev.mrt@gmail.com>
2023-09-04 14:31:22 +0500

    update alerts_frame_windows_locators.py, alerts_frame_windows_page.py, alerts_frame_windows_test.py added class: AlertsPage added tests: test_see_alert, test_alert_appear_5_sec, test_confirm_alert,test_prompt_alert

--- locators/alerts_frame_windows_locators.py
+++ locators/alerts_frame_windows_locators.py
@@ -5,3 +5,12 @@ class BrowserWindowsPageLocators:
     NEW_TAB_BUTTON = (By.CSS_SELECTOR, "#tabButton")
     NEW_WINDOW_BUTTON = (By.CSS_SELECTOR, "#windowButton")
     TITLE_NEW = (By.CSS_SELECTOR, "#sampleHeading")
+
+
+class AlertsPageLocators:
+    SEE_ALERT_BUTTON = (By.CSS_SELECTOR, "#alertButton")
+    APPEAR_ALERT_AFTER_5_SEC_BUTTON = (By.CSS_SELECTOR, "#timerAlertButton")
+    CONFIRM_BOX_ALERT_BUTTON = (By.CSS_SELECTOR, "#confirmButton")
+    CONFIRM_RESULT = (By.CSS_SELECTOR, "#confirmResult")
+    PROMPT_BOX_ALERT_BUTTON = (By.CSS_SELECTOR, "#promtButton")
+    PROMPT_RESULT = (By.CSS_SELECTOR, "#promptResult")
--- pages/alerts_frame_windows_page.py
+++ pages/alerts_frame_windows_page.py
@@ -1,6 +1,9 @@
+import random
 import time
 
-from locators.alerts_frame_windows_locators import BrowserWindowsPageLocators
+from selenium.common import UnexpectedAlertPresentException
+
+from locators.alerts_frame_windows_locators import BrowserWindowsPageLocators, AlertsPageLocators
 from pages.base_page import BasePage
 
 
@@ -18,3 +21,37 @@ class BrowserWindowsPage(BasePage):
         text_title = self.element_is_present(self.locators.TITLE_NEW).text
         return text_title
 
+
+class AlertsPage(BasePage):
+    locators = AlertsPageLocators()
+
+    def check_see_alert(self):
+        self.element_is_visible(self.locators.SEE_ALERT_BUTTON).click()
+        alert_window = self.driver.switch_to.alert
+        return alert_window.text
+
+    def check_alert_appear_5_sec(self):
+        self.element_is_visible(self.locators.APPEAR_ALERT_AFTER_5_SEC_BUTTON).click()
+        time.sleep(6)
+        try:
+            alert_window = self.driver.switch_to.alert
+            return alert_window.text
+        except UnexpectedAlertPresentException:
+            alert_window = self.driver.switch_to.alert
+            return alert_window.text
+
+    def check_confirm_alert(self):
+        self.element_is_visible(self.locators.CONFIRM_BOX_ALERT_BUTTON).click()
+        alert_window = self.driver.switch_to.alert
+        alert_window.accept()
+        text_result = self.element_is_present(self.locators.CONFIRM_RESULT).text
+        return text_result
+
+    def check_prompt_alert(self):
+        text = f"autotest{random.randint(0, 999)}"
+        self.element_is_visible(self.locators.PROMPT_BOX_ALERT_BUTTON).click()
+        alert_window = self.driver.switch_to.alert
+        alert_window.send_keys(text)
+        alert_window.accept()
+        text_result = self.element_is_present(self.locators.PROMPT_RESULT).text
+        return text, text_result
