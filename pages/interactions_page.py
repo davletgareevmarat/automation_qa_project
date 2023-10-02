0b5c7c691c22400aff1f0c38cf127c5f4ba1c065
davletgareevmarat <davletgareev.mrt@gmail.com>
2023-10-02 10:23:58 +0500

    create interactions_page.py, interactions_test.py, interacitions_page_locators.py added TestSortablePage, TestSelectablePage

--- pages/interactions_page.py
+++ pages/interactions_page.py
@@ -1,6 +1,9 @@
 import random
 
-from locators.interacitions_page_locators import SortablePageLocators, SelectablePageLocators
+from locators.interacitions_page_locators import (
+    SortablePageLocators,
+    SelectablePageLocators,
+)
 from pages.base_page import BasePage
 
 
@@ -14,7 +17,9 @@ class SortablePage(BasePage):
     def change_list_order(self):
         self.element_is_visible(self.locators.TAB_LIST).click()
         order_before = self.get_sortable_items(self.locators.LIST_ITEM)
-        item_list = random.sample(self.elements_are_visible(self.locators.LIST_ITEM), k=2)
+        item_list = random.sample(
+            self.elements_are_visible(self.locators.LIST_ITEM), k=2
+        )
         item_what = item_list[0]
         item_where = item_list[1]
         self.action_drag_and_drop_to_element(item_what, item_where)
@@ -24,7 +29,9 @@ class SortablePage(BasePage):
     def change_grid_order(self):
         self.element_is_visible(self.locators.TAB_GRID).click()
         order_before = self.get_sortable_items(self.locators.GRID_ITEM)
-        item_list = random.sample(self.elements_are_visible(self.locators.GRID_ITEM), k=2)
+        item_list = random.sample(
+            self.elements_are_visible(self.locators.GRID_ITEM), k=2
+        )
         item_what = item_list[0]
         item_where = item_list[1]
         self.action_drag_and_drop_to_element(item_what, item_where)
